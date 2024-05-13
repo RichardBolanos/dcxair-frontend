1e68448ed1bc4e6193ef8cbca873ef69936eb28a
Admorab-Ing1 <richarandres1998@hotmail.com>
2024-05-13 17:25:18 -0500

    feat: 🚀 initialize Angular project
    ✨ Created a new Angular project using Angular CLI.
    🛠 Configured initial dependencies and scripts.
    🔧 Set up basic routing and project structure.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { FlightsComponent } from './pages/flights/flights.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'flights',
+    pathMatch: 'full',
+  },
+  {
+    path: 'flights',
+    component: FlightsComponent,
+    // canActivate: [isLoggedIn],
+    // children: [
+    //   { path: '', redirectTo: 'tickets', pathMatch: 'full' },
+    // ],
+  }
+];
---
+++ src/environments/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+    rapidAPIKey: "177f567d19msh04279c070b21d6ep133bd6jsn310508d48c16",
+    rapidAPIHost: "airport-info.p.rapidapi.com",
+    dcxairService: "http://localhost:5217/",
+    flightsServiceRoute: "Flight/get-flights",
+    flightsCountriesService: "Flight/get-countries"
+};
